0f914a1c3e2765823bc4c7abc3241371406158b4
Gemagit <gemitaphotographer@gmail.com>
2024-02-15 00:29:34 +0100

    modified index.html

---
+++ index.js
@@ -0,0 +1,189 @@
+let Coche = {
+    marca:"",
+    modelo:"",
+    matricula:"",
+}
+
+
+let Casa = {
+    codPostal:"",
+    calle:"",
+    portal:"",
+    piso:"",
+}
+
+
+let FullStackDeveloper = {
+    lenguajes:[],
+    proyectos: [],
+}
+
+
+let Perro = {
+    nombre :"",
+    raza: "",
+    color:"",
+    edad:"",
+    ladrar : function() {
+        console.log("");
+    },
+    popo: function () {
+        return Math.random()*3;
+    }
+}
+
+/*let Portatil ={
+    marca:"",
+};
+let marcaPortatil= Portatil.marca;
+
+
+let Portatil2 =[
+    {marca:"Linux"}
+]
+let marcaPortatil2= Portatil2[0].marca;
+
+
+let concierto = [
+    {grupos:""},
+]
+let grupos= concierto[0].grupos;
+
+
+
+let Led =[
+    {rojo,verde,azul},
+]
+let Rgb= [rojo,verde,azul];
+
+
+let Portatil3 ={
+    modelo:"Alfa",
+    marca:"Lg",
+    color:"gris",
+    pulgadas:"9",
+}
+Portatil.modelo= "P354";
+
+
+let Concierto1= {
+    fecha:"diciembre",
+    localización:"pabellón sur",
+}
+Concierto.fecha= newDate("25 de septiembre");*/
+
+
+let Noticia = {
+    titular:"",
+    cuerpo:"",
+}
+
+
+
+let Persona= {
+    nombre:"",
+    apellidos:"",
+    edad :"edad",
+}
+
+
+
+let Avion= {
+    numPasajeros:"",
+    despegar: function() {
+        console.log("Despegando");
+    },
+    volar: function() {
+        console.log("Llegando al destino");
+    },
+    aterrizar: function() {
+        console.log("Aterrizando");
+    },
+}
+
+
+let Paquete= [
+    {color: "marrón"},
+    {tamaño:"mediano"},
+    {forma:"cuadrada"}
+]
+
+
+let Pais= {
+    numHabitantes:"",
+    continente:"",
+    gentilicio:"",
+}
+
+
+
+
+/*let Grupo ={
+    numIntegrantes:"8",
+    nacionalidad:"española",
+ }
+ Grupo.numIntegrantes= "5";
+
+
+
+
+ let Pantalla ={
+    dimensiones:"300x400",
+    color:"azul",
+ }
+ Pantalla.dimensiones="1920x1080";
+
+
+
+
+ let led ={
+    encendido:"true",
+ }
+ led.encendido="false";
+
+
+
+
+ let Movil={
+    temperatura:"80º",
+    color:"gris",
+ }
+ Movil.temperatura= "20º";
+
+
+let O_Error= {
+    codigo: "",
+}
+let codError= O_Error.codigo;
+
+
+
+
+
+let Grupo = [
+    {integrantes:"6",nacionalidad:"francesa"},
+]
+let integrantes= Grupo[0].integrantes;
+
+
+
+let Impresora = [
+    {tinta1:"rojo",tinta2:"verde",tinta3:"azul"},
+]
+let nivelesTinta= Impresora[0].tinta;
+
+
+
+let Pantalla =[
+    {tamaño:"568x379",pixeles:"14"},
+]
+let pixeles= Pantalla[1].pixeles;
+
+
+
+
+
+let Movil =[
+    {modelo:"samsung",especificaciones:"waterproof",tamaño:"30x70"},
+]
+let especificaciones= Movil[1].especificaciones;*/
